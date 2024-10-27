20d27ad2c80dc07d7d299cde755609a2fbdfd155
tk-stardust <1318141447@qq.com>
2024-10-28 01:42:22 +0800

    aside

---
+++ src/components/aside.vue
@@ -0,0 +1,43 @@
+<template>
+    
+ <el-menu 
+    active-text-color="#ffd04b" 
+    background-color="#545c64" 
+    class="aside-container" 
+    default-active="2"
+    text-color="#fff" 
+    @open="handleOpen" 
+    @close="handleClose"
+    >
+        <p class="logo-lg">TK陪诊</p>
+        <TreeMenu :menuData/>
+    </el-menu>
+</template>
+
+<script setup>
+import TreeMenu from "./treeMenu.vue"
+import { useRouter } from "vue-router";
+import { reactive } from "vue";
+const router = useRouter()
+const menuData = reactive(router.options.routes[0].children)
+const handleOpen = ()=>{
+
+}
+const handleClose = () =>{
+
+}
+</script>
+
+<style lang="less" scoped>
+.aside-container{
+    height: 100%;
+    .logo-lg{
+        font-size: 20px;
+        text-align: center;
+        height: 50px;
+        line-height: 50px;
+        color: #fff;
+    }
+}
+
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>main</div>
+  <div class="common-layout">
+    <el-container>
+      <!-- <el-aside width="200px">Aside</el-aside> -->
+      <Aside />
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script setup>
-
+import Aside from '../components/aside.vue';
 </script>
 
-<style>
+<style  lang="less" scoped>
+.common-layout{
+  height: 100%;
+  .el-container{
+    height: 100%;
+  }
+}
 
 </style>
