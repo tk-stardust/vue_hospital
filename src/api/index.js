275ad8d2bec92b83ac2ec874a3b82844cbbbec39
tk-stardust <1318141447@qq.com>
2024-11-02 21:02:04 +0800

    表单验证和axios二次封装

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+import request from '../utils/request'
+// 发送验证码
+export const getCode = (data) =>{
+    return request.post('/get/code',data)
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,153 @@
 <template>
-  
+    <el-row class="login-container" justify="center" align="middle">
+        <el-card style="max-width: 480px;">
+            <template #header>
+                <div class="card-header">
+                    <img :src="imgUrl" alt="">
+                </div>
+            </template>
+            <div class="jump-link">
+                <el-link type="primary" @click="handleChange">{{ formType ? "返回登录" : "注册账号" }}</el-link>
+            </div>
+            <el-form 
+            :model="loginForm" 
+            style="max-width: 600px;" 
+            class="demo-ruleForm"
+            :rules="rules">
+                <el-form-item prop="userName">
+                    <el-input v-model="loginForm.userName" placeholder="手机号" prefix-icon="UserFilled"></el-input>
+                </el-form-item>
+                <el-form-item prop="passWord">
+                    <el-input v-model="loginForm.passWord" placeholder="密码" prefix-icon="Lock"></el-input>
+                </el-form-item>
+                <el-form-item v-if="formType" prop="validCode">
+                    <el-input v-model="loginForm.validCode" placeholder="验证码" prefix-icon="Lock">
+                        <template #append>
+                            <span @click="countdownChange">{{ countdown.validText }}</span>
+                        </template>
+                    </el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" :style="{width: '100%'}" @click="submitForm">
+                        {{ formType ? '注册账号' : '登录' }}
+                    </el-button>
+                </el-form-item>
+            </el-form>
+        </el-card>
+    </el-row>
 </template>
 
 <script setup>
+import { reactive, ref } from 'vue';
+import { getCode } from '../../api'
+import { ElMessage } from 'element-plus';
+
+const imgUrl = new URL("../../../public/login-head.png",import.meta.url).href 
+const phoneReg = /^1(3[0-9]|4[01456879]|5[0-35-9]|6[2567]|7[0-8]|8[0-9]|9[0-35-9])\d{8}$/
+const passReg = /^[a-zA-Z0-9_-]{4,16}$/
+
+// 表单数据
+const loginForm = reactive({
+    userName:'',
+    passWord:'',
+    validCode:''
+})
+
+// 切换表单(0登录 1注册)
+const formType = ref(0)
+
+// 点击切换登录和注册
+const handleChange = ()=>{
+    formType.value = formType.value ? 0 : 1
+}
+// 账号校验规则
+const validateUser = (rule,value,callback) => {
+    // 不为空
+    if(value === ''){
+        callback(new Error('请输入账号'))
+    }else{
+        phoneReg.test(value) ? callback() :callback(new Error('手机号格式不对，请输入正确手机号'))
+    }
+}
+
+// 密码校验
+const validatePass = (rule,value,callback) => {
+    // 不为空
+    if(value === ''){
+        callback(new Error('请输入密码'))
+    }else{
+        passReg.test(value) ? callback() : callback(new Error('密码格式不对，请输入4-16位字符'))
+    }
+}
+
+
+// 表单校验
+const rules = reactive({
+    userName: [{validator: validateUser, trigger:'blur'}],
+    passWord: [{validator: validatePass, trigger:'blur'}]
+})
+
+// 发送短信
+const countdown = reactive({
+    validText:'获取验证码',
+    time:3
+})
+let flag = false
+
+const countdownChange = ()=>{
+    // 如果已发送不处理
+    if(flag)return
+    // 判断手机号是否正确
+    if(!loginForm.userName || !phoneReg.test(loginForm.userName)){
+        return ElMessage({
+            message:'请检查手机号是否正确',
+            type:'warning'
+        })
+    }
+    // 倒计时
+    let interval = setInterval(()=>{
+        if(countdown.time<=0){
+            countdown.time = 3
+            countdown.validText = '获取验证码'
+            flag = false
+            clearInterval(interval)
+        }else{
+            countdown.time -= 1
+            countdown.validText = `剩余${countdown.time}s`
+
+        }
+    },1000)
+    flag = true
+    getCode({tel:loginForm.userName}).then(({data}) => {
+
+        if(data.code === 10000){
+            ElMessage.success('发送成功')
+        }
+    })
+}
+
+// 表单提交
+const submitForm = ()=>{
+
+}
 
 </script>
 
-<style>
-
+<style lang="less" scoped>
+:deep(.el-card__header) {
+    padding: 0
+  }
+  .login-container {
+    height: 100%;
+    .card-header{
+      background-color: #899fe1;
+      img {
+        width: 430px;
+      }
+    }
+    .jump-link {
+      text-align: right;
+      margin-bottom: 10px;
+    }
+  }
 </style>
